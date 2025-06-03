f350b5ffbfffbcd8abcfc1c99458e26dc0d80220
T-jibbe <tjibbe.van.ommeren.cornelis@gmail.com>
2025-06-03 12:10:27 +0200

    V1

---
+++ RaidingGens/src/main/java/Tjibbe_2007/com/raidingGens/RaidingGens.java
@@ -0,0 +1,21 @@
+package Tjibbe_2007.com.raidingGens;
+
+import Tjibbe_2007.com.raidingGens.Commands.GeneratorCommand;
+import Tjibbe_2007.com.raidingGens.Listeners.BlockHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RaidingGens extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new BlockHandler(), this);
+
+        this.getCommand("generator").setExecutor(new GeneratorCommand());
+
+        this.getLogger().info("Successfully enabled");
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info("Successfully disabled");
+    }
+}
